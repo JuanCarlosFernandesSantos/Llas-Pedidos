4c6e51f6c973fe9e0bc2c87bdd77b0f3eb9d17fd
Leone Oliveira <oliveiraleone75@gmail.com>
2022-10-04 22:31:18 -0300

    Criando a o posso de mudar de tela e a classe TelaInicial

--- app/src/main/java/com/llas_pedidos/MainActivity.java
+++ app/src/main/java/com/llas_pedidos/MainActivity.java
@@ -2,17 +2,30 @@ package com.llas_pedidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_inicialc);
+        setContentView(R.layout.activity_main);
+
+        botaoProximaTela();
     }
 
+    private void botaoProximaTela() {
+        Button proximoBotao = (Button) findViewById(R.id.btEntrar);
+        proximoBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TelaInicial.class));
+            }
+        });
 
-
+    }
 
 }
