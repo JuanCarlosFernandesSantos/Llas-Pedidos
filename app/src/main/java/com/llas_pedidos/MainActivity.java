384d0403330e4353f510b50f610e4d9205a82d97
Juan Carlos <juancfdosantos@gmail.com>
2022-10-04 22:23:26 -0300

    ATT_CADASTRO'

--- app/src/main/java/com/llas_pedidos/MainActivity.java
+++ app/src/main/java/com/llas_pedidos/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tela_inicialm);
+        setContentView(R.layout.tela_inicialc);
     }
 
 
